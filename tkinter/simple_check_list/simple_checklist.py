891243821a1d8ac1678a3b3b1a55b1e3c7ecb54e
SvetlanaDShopova <92042506+SvetlanaDShopova@users.noreply.github.com>
2023-12-15 12:24:45 -0500

    Fix the spacing

--- tkinter/simple_check_list/simple_checklist.py
+++ tkinter/simple_check_list/simple_checklist.py
@@ -31,47 +31,51 @@ def create_button(frame, text,comm="something"):
     return btn
 
 def entry_return(event):
-    '''Press "Add Item" button programetically when <Enter>
-    is prssed in entry field'''
+    """Press "Add Item" button programetically when <Enter>
+    is prssed in entry field"""
     btn_list_add_item.invoke()
 
 
 def add_item():
-    '''Adding a new task to the list'''
+    """Adding a new task to the list"""
     item = list_entry.get().strip()
     if item and  item != "":
         my_listbox.insert(END, item)
         list_entry.delete(0,END)
     
 def remove_item():
-    '''Remove selected(anchor) item from the list'''
+    """Remove selected(anchor) item from the list"""
     my_listbox.delete(ANCHOR)
     
 def clear_list():
-    '''Clear all items from the list'''
+    """Clear all items from the list"""
     my_listbox.delete(0,END)
 
 def save_list():
-    '''Save the list into a simple txt file'''
+    """Save the list into a simple txt file"""
     #open file to write
     with open('checklist.txt', 'w')as f:
         #listbox.get() returns a tuple....
         list_tuple = my_listbox.get(0, END)        
         print(list_tuple, file=f) 
-        f.close()
-    btn_clear_list.invoke()
+        f.close()    
  
 def open_list():
-    '''Open saved list'''
-    my_saved_list=()
-    with open('checklist.txt', 'r')as f:
-        lines = f.readlines()        
-        data = [tuple(line.strip('()').strip("'").split(',')) for line in lines]
-        my_saved_list = data[0] 
-        if len(my_saved_list) > 0:
-            for task in my_saved_list:               
-                my_listbox.insert(END, task.strip("'").strip(" ").replace("'", "").replace(")", ""))
-    f.close()
+    """Open saved list"""
+    try:
+        my_saved_list=()
+        with open('checklist.txt', 'r')as f:
+            lines = f.readlines()        
+            data = [tuple(line.strip('()').strip("'").split(',')) for line in lines]
+            my_saved_list = data[0] 
+            if len(my_saved_list) > 0:
+                for task in my_saved_list: 
+                    if task.strip() != "":
+                        my_listbox.insert(END, task.replace("'", "").replace(")", "")
+                                          .replace("\\n","").strip())
+        f.close()
+    except:
+         return
 
 ####### Define layout ############
 
@@ -85,16 +89,16 @@ output_frame.pack()
 button_frame.pack()
 
 #Input frame layout
-list_entry = tkinter.Entry(input_frame, width=34, borderwidth=3,
+list_entry = tkinter.Entry(input_frame, width=46, borderwidth=3,
                            font=my_font)
 btn_list_add_item =create_button(input_frame, text="Add Item", comm=add_item)
 
-btn_open_list = create_button(input_frame, text="Open List", comm=open_list)
 
-btn_open_list.grid(row=0, column=0, padx=5 )
-list_entry.grid(row=0, column=1, padx=5, pady=10)
+
+
+list_entry.grid(row=0, column=0, padx=5, pady=10)
 list_entry.bind("<Return>", entry_return)
-btn_list_add_item .grid(row=0, column=2, padx=5)
+btn_list_add_item .grid(row=0, column=1, padx=5)
 
 
 
@@ -122,7 +126,8 @@ btn_clear_list.grid(row=0, column=1, padx=10, ipadx=10)
 btn_save_list.grid(row=0, column=2, padx=10, ipadx=10)
 btn_quit.grid(row=0, column=3, padx=10, ipadx=10)
 
-
+#open list if available
+open_list()
 #run main loop
 
 root.mainloop()
