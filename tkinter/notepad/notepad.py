c174051b9282b81ee407d985c3c5efd09cb91777
SvetlanaDShopova <92042506+SvetlanaDShopova@users.noreply.github.com>
2024-01-31 19:32:04 -0500

    Add more buttons and finish menu frame

--- tkinter/notepad/notepad.py
+++ tkinter/notepad/notepad.py
@@ -6,7 +6,7 @@ Created on Tue Jan 23 16:53:13 2024
 """
 import tkinter
 from PIL import ImageTk, Image
-from tkinter import StringVar, IntVar, scrolledtext, END, messagebox, filedialog, ttk, PhotoImage
+from tkinter import StringVar, BOTH, IntVar, scrolledtext, END, messagebox, filedialog, ttk, PhotoImage
 
 ########## Define window ##########
 root = tkinter.Tk()
@@ -18,39 +18,35 @@ root.config(bg="#D5D7D5")
 
 ########## Define fonts and colors ##########
 text_color = "#161811"
-menu_color = "#989C94"
-
+menu_color = "#EAEBEA"
+dropdown_font=("Areal", 9)
 ########## Define dropdown list values #########
-font_family = ['Terminal', 'Modern', 'Script', 'Courier', 'Arial', 'Calibri', 'Cambria',
+families = ['Consolas','Terminal', 'Modern', 'Script', 'Courier', 'Arial', 'Calibri', 'Cambria',
 'Georgia', 'MS Gothic', 'SimSun', 'Tahoma', 'Times New Roman', 'Verdana', 'Wingdings']
-font_options = ['none', 'bold', 'italic']
-font_size = [8, 10, 12, 14, 16, 20, 24, 32, 48, 64, 72, 96]
+font_options = ['normal', 'bold', 'italic']
+sizes = [8, 10, 12, 14, 16, 20, 24, 32, 48, 64, 72, 96]
 ########### Define functions ##########  
 
-def create_dropdown(list_values, r=0, c=0):
+def create_dropdown(list_values, r=0, c=0, width=0):
     """Create dropdown menues for font family, font size and type"""
-    dropdown = ttk.Combobox(menu_frame, value=list_values, width=13,
-              font=text_color, justify='center', background=menu_color)
-    dropdown.option_add('*TCombobox*Listbox.Justify', 'center')
+    dropdown = ttk.Combobox(menu_frame, value=list_values, width=width,
+              font=dropdown_font, justify='center', background=menu_color)
+    dropdown.option_add('*TCombobox*Listbox.Justify', 'left')
     dropdown.set(list_values[0])
     
-    dropdown.grid(row=r, column=c, padx=5, pady=5)    
+    dropdown.grid(row=r, column=c, padx=3, pady=5)    
     return dropdown
 ########## Define Layout ##########
 
 ### Frames ###
-menu_frame = tkinter.Frame(root, bg=menu_color)
+menu_frame = tkinter.Frame(root, bg=menu_color, width=500)
 text_frame = tkinter.Frame(root, bg=text_color)
 
-menu_frame.grid(row=0, column=0, padx=5, pady=5)
-text_frame.grid(row=1, column=0,padx=5, pady=5)
+menu_frame.pack(  pady=(0,5), fill=BOTH)
+text_frame.pack(padx=5, pady=5)
 
 
 ### Layout for menu frame ###
-
-#button new
-
-#btn_new = create_button("new.png", 0, 0)
 
 img_new = ImageTk.PhotoImage(Image.open("new.png"))  
 btn_new =  tkinter.Button(menu_frame, image=img_new, height=30, width=30,
@@ -58,20 +54,55 @@ btn_new =  tkinter.Button(menu_frame, image=img_new, height=30, width=30,
 btn_new.grid(row = 0, column=0, padx=5, pady=5)
 
 img_open = ImageTk.PhotoImage(Image.open("open.png"))  
-btn_open =  tkinter.Button(menu_frame, image=img_open, height=30, width=30,
+btn_open = tkinter.Button(menu_frame, image=img_open, height=30, width=30,
         bg=menu_color, borderwidth=0, activebackground = menu_color)
 btn_open.grid(row = 0, column=1, padx=5, pady=5)
 
 img_save = ImageTk.PhotoImage(Image.open("save.png"))  
-btn_save =  tkinter.Button(menu_frame, image=img_save, height=30, width=30,
+btn_save = tkinter.Button(menu_frame, image=img_save, height=30, width=30,
         bg=menu_color, borderwidth=0, activebackground = menu_color)
 btn_save.grid(row = 0, column=2, padx=5, pady=5)
 
-box_font_family = create_dropdown(font_family, 0, 3)
+img_print = ImageTk.PhotoImage(Image.open("print.png"))  
+btn_print = tkinter.Button(menu_frame, image=img_print, height=30, width=30,
+        bg=menu_color, borderwidth=0, activebackground = menu_color)
+btn_print.grid(row = 0, column=3, padx=5, pady=5)
 
-box_font_size = create_dropdown(font_size, 0, 4)
+styl = ttk.Style()
+styl.configure('TSeparator', background='#000000')
+separator = ttk.Separator(menu_frame, orient='vertical', style='TSeparator')
+separator.grid(row=0, column=4, padx=5, pady=5, sticky="ns")
 
-box_font_options = create_dropdown(font_options, 0, 5)
+img_cut = ImageTk.PhotoImage(Image.open("cut.png"))  
+btn_cut =  tkinter.Button(menu_frame, image=img_cut, height=30, width=30,
+        bg=menu_color, borderwidth=0, activebackground = menu_color)
+btn_cut.grid(row = 0, column=5, padx=5, pady=5)
+
+img_copy = ImageTk.PhotoImage(Image.open("copy.png"))  
+btn_copy = tkinter.Button(menu_frame, image=img_copy, height=30, width=30,
+        bg=menu_color, borderwidth=0, activebackground = menu_color)
+btn_copy.grid(row = 0, column=6, padx=5, pady=5)
+
+img_paste = ImageTk.PhotoImage(Image.open("paste.png"))  
+btn_paste = tkinter.Button(menu_frame, image=img_paste, height=30, width=30,
+        bg=menu_color, borderwidth=0, activebackground = menu_color)
+btn_paste.grid(row = 0, column=7, padx=5, pady=5)
+
+
+separator2 = ttk.Separator(menu_frame, orient='vertical', style='TSeparator')
+separator2.grid(row=0, column=8, padx=5, pady=5, sticky="ns")
+
+font_family = StringVar()
+font_family.set(families[0])
+box_font_family = create_dropdown(families, 0, 9, width=16)
+
+font_size=IntVar()
+font_size.set(sizes[0])
+box_font_size = create_dropdown(sizes, 0, 10, width = 4)
+
+font_option = StringVar()
+font_option.set(font_options[0])
+box_font_options = create_dropdown(font_options, 0, 11, width= 7)
 
 #Run the root's main loop
 root.mainloop()
